e03d28e1075a6f0ccc6936ac749b03bf59ff2e5b
git478345960 <478345960@qq.com>
2018-12-03 23:28:44 +0800

    4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Person from './person/Person'
 
@@ -8,8 +7,12 @@ class App extends Component {
       persons:[
         {name : '李大妈',count:50},
         {name : '李大发',count: 80},
-        {name : 'dads',count: 90}
-      ]
+        {name : 'dads',count: 90},
+        {name : '李大发111',count: 80},
+        {name : 'dads111',count: 90},
+        {name : 'dads111',count: 90},
+      ],
+     showPersons: null,
   }
   
   change = (newName) =>{
@@ -21,26 +24,88 @@ class App extends Component {
       ]
     })
   }
+
   changeName = (event) =>{
     this.setState({
       persons:[
         {name : event.target.value,count:50},
         {name : '李大发111',count: 80},
-        {name : 'dads111',count: 90}
+        {name : 'dads111',count: 90},
+        {name : 'dads111',count: 90},
+
       ]
     })
   }
+  switchNameHandler = () =>{
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow})
+  }
   render() {
-    return (
 
+    let persons = null;
+    if(this.state.showPersons){
+      persons = (
+        <div>
+        {
+          this.state.persons.map((person,index) => {
+            return <Person name = {person.name} count = {person.count} key = {index} />
+          })
+        }
+
+
+        {/* <Person 
+        changeName = {this.changeName}
+        name ={this.state.persons[0].name} 
+        count = {this.state.persons[0].count}/>
+        <Person 
+        myclick = {this.change.bind(this,"米修mussu")} 
+        name ={this.state.persons[1].name} 
+        count = {this.state.persons[1].count}/>
+        <Person 
+        name ={this.state.persons[2].name} 
+        count = {this.state.persons[2].count}/> */}
+          </div>
+      )
+    }
+    return (
+      
       <div>
           <div>手动阀手动阀</div>
+          <button onClick = {this.switchNameHandler}>show/hide</button>
+          {persons}
+          {/* {
+
+            this.state.showPersons  === true ?
+            <div>
           <Person 
           changeName = {this.changeName}
-          name ={this.state.persons[0].name} count = {this.state.persons[0].count}/>
-          <Person myclick = {this.change.bind(this,"米修mussu")} name ={this.state.persons[1].name} count = {this.state.persons[1].count}/>
-          <Person name ={this.state.persons[2].name} count = {this.state.persons[2].count}/>
-          <button onClick = {this.change.bind(this,'mussu')}>click me</button>
+          name ={this.state.persons[0].name} 
+          count = {this.state.persons[0].count}/>
+          <Person 
+          myclick = {this.change.bind(this,"米修mussu")} 
+          name ={this.state.persons[1].name} 
+          count = {this.state.persons[1].count}/>
+          <Person 
+          name ={this.state.persons[2].name} 
+          count = {this.state.persons[2].count}/>
+            </div>
+            : null
+          } */}
+          
+          {/* <Person 
+          changeName = {this.changeName}
+          name ={this.state.persons[0].name} 
+          count = {this.state.persons[0].count}/>
+          <Person 
+          myclick = {this.change.bind(this,"米修mussu")} 
+          name ={this.state.persons[1].name} 
+          count = {this.state.persons[1].count}/>
+          <Person 
+          name ={this.state.persons[2].name} 
+          count = {this.state.persons[2].count}/>
+          </div> */}
+          {/* <button onClick = {this.change.bind(this,'mussu')}>click me</button> */}
+
       </div>
       
     );
